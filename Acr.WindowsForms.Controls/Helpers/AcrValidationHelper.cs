5bd32f3f6255a0c9755e220330e26463ee61fce5
Arthur PC ELIAS <pacrbr60@gmail.com>
2026-02-07 14:28:37 -0300

    feat: IAcrValidatableControl -> Interface para auto validação do controle feat: AcrValidationHelper -> Helper para ajudar a validar o controle

---
+++ Acr.WindowsForms.Controls/Helpers/AcrValidationHelper.cs
@@ -0,0 +1,21 @@
+﻿using Acr.WindowsForms.Controls.Interfaces;
+using System.ComponentModel;
+
+namespace Acr.WindowsForms.Controls.Helpers;
+
+public static class AcrValidationHelper
+{
+    public static void ValidateRequired(IAcrValidatableControl control, bool blockLeave, CancelEventArgs? e = null)
+    {
+        if (!control.RequiredField)
+            return;
+
+        if (control.IsControlEmpty)
+        {
+            control.ShowRequiredFieldError();
+            if (blockLeave && e != null)
+                e.Cancel = true;
+        }
+        else control.ClearError();
+    }
+}
---
+++ Acr.WindowsForms.Controls/Interfaces/IAcrValidatableControl.cs
@@ -0,0 +1,23 @@
+﻿namespace Acr.WindowsForms.Controls.Interfaces;
+
+public interface IAcrValidatableControl
+{
+    bool BlockLeave { get; set; }
+    bool RequiredField { get; set; }
+    string WarningMessageRequiredField { get; set; }
+
+    /// <summary>
+    /// Retorna se o controle está "vazio"
+    /// </summary>
+    bool IsControlEmpty { get; }
+
+    /// <summary>
+    /// Mostra a mensagem de erro (Normalmente vai usar o LabelHelper)
+    /// </summary>
+    void ShowRequiredFieldError();
+
+    /// <summary>
+    /// Remove a mensagem de erro
+    /// </summary>
+    void ClearError();
+}
