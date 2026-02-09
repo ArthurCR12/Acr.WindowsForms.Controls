20aaf917c61019a61fba4b868c6e8dc8a09248df
Arthur PC ELIAS <pacrbr60@gmail.com>
2026-02-09 19:10:11 -0300

    feat: AcrDataGridView -> Com o ControlState

---
+++ Acr.WindowsForms.Controls/Controls/CustomDataGridView/AcrDataGridView.cs
@@ -0,0 +1,40 @@
+﻿using Acr.WindowsForms.Controls.Enums;
+using Acr.WindowsForms.Controls.Interfaces;
+using System.ComponentModel;
+
+namespace Acr.WindowsForms.Controls.Controls.CustomDataGridView;
+
+public class AcrDataGridView : DataGridView, IAcrBaseControl
+{
+    private EControlState _controlState = EControlState.Normal;
+    [Category("Acr Custom")]
+    [Browsable(true)]
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+    public EControlState ControlState
+    {
+        get => _controlState;
+        set
+        {
+            if (_controlState == value) return;
+            _controlState = value;
+            ApplyState();
+        }
+    }
+
+    public void ApplyState()
+    {
+        switch (_controlState)
+        {
+            case EControlState.Normal:
+                ReadOnly = false;
+                Enabled = true; break;
+            case EControlState.Disabled:
+                Enabled = false; break;
+            case EControlState.ReadOnly:
+                ReadOnly = true; break;
+            case EControlState.Edit:
+                ReadOnly = false;
+                Enabled = true; break;
+        }
+    }
+}
