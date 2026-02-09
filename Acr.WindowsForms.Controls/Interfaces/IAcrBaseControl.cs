afd45ccad461e3df5c4754ef7dddb596e0c1ba11
Arthur PC ELIAS <pacrbr60@gmail.com>
2026-02-09 18:28:54 -0300

    feat: Interface para a base de todos os controles, começando com o ControlState

---
+++ Acr.WindowsForms.Controls/Interfaces/IAcrBaseControl.cs
@@ -0,0 +1,10 @@
+﻿using Acr.WindowsForms.Controls.Enums;
+
+namespace Acr.WindowsForms.Controls.Interfaces;
+
+public interface IAcrBaseControl
+{
+    EControlState ControlState { get; set; }
+
+    void ApplyState();
+}
