1d1e6dc2a82f981103af0f1e2f50bc309be222f8
Arthur PC ELIAS <pacrbr60@gmail.com>
2026-02-09 15:17:56 -0300

    feat: AcrComboBox -> Nada de mais, só o controle com o  Field Required

---
+++ Acr.WindowsForms.Controls/Controls/CustomComboBox/AcrComboBox.cs
@@ -0,0 +1,57 @@
+﻿using Acr.WindowsForms.Controls.Class;
+using Acr.WindowsForms.Controls.Enums;
+using Acr.WindowsForms.Controls.Interfaces;
+using System.ComponentModel;
+
+namespace Acr.WindowsForms.Controls.Controls.CustomComboBox;
+
+public class AcrComboBox : ComboBox, IAcrValidatableControl
+{
+    private bool _requiredField = false;
+    private bool _blockLeave = false;
+    private string _warningMessageRequiredField = "This field is required!";
+
+    public bool IsControlEmpty => SelectedIndex == -1;
+
+
+    [Category("Acr Custom")]
+    [Description("If true, the field is marked as required.")]
+    [Browsable(true)]
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+    public bool RequiredField
+    {
+        get => _requiredField;
+        set => _requiredField = value;
+    }
+
+    [Category("Acr Custom")]
+    [Description("Message for a Required Field")]
+    [Browsable(true)]
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+    public string WarningMessageRequiredField
+    {
+        get => _warningMessageRequiredField;
+        set => _warningMessageRequiredField = value;
+    }
+
+    [Category("Acr Custom")]
+    [Description("If true, the control will block leaving when validation fails.")]
+    [Browsable(true)]
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+    public bool BlockLeave
+    {
+        get => _blockLeave;
+        set => _blockLeave = value;
+    }
+    
+
+    public void ClearError()
+    {
+        LabelHelper.RemoveLabel(this, MessageType.Error);
+    }
+
+    public void ShowRequiredFieldError()
+    {
+        LabelHelper.CreateLabel(this, WarningMessageRequiredField, MessageType.Error);
+    }
+}
