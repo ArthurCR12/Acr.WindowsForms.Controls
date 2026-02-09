029bac18c171665b5a22511506064f8fa31de489
Arthur PC ELIAS <pacrbr60@gmail.com>
2026-02-09 19:10:24 -0300

    feat: AcrButton -> Com o ControlState

---
+++ Acr.WindowsForms.Controls/Controls/CustomButton/AcrButton.cs
@@ -0,0 +1,39 @@
+﻿using Acr.WindowsForms.Controls.Enums;
+using Acr.WindowsForms.Controls.Interfaces;
+using System.ComponentModel;
+
+namespace Acr.WindowsForms.Controls.Controls.CustomButton;
+
+public class AcrButton : Button, IAcrBaseControl
+{
+    private EControlState _controlState = EControlState.Normal;
+
+    [Category("Acr Custom")]
+    [Browsable(true)]
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+    public EControlState ControlState
+    {
+        get => _controlState;
+        set
+        {
+            if (_controlState == value) return;
+            _controlState = value;
+            ApplyState();
+        }
+    }
+
+    public void ApplyState()
+    {
+        switch (_controlState)
+        {
+            case EControlState.Normal:                
+                Enabled = true; break;
+            case EControlState.Disabled:
+                Enabled = false; break;
+            case EControlState.ReadOnly:
+                Enabled = false; break;
+            case EControlState.Edit:
+                Enabled = true; break;
+        }
+    }
+}
